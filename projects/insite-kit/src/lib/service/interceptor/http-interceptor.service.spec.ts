88547e7a10da5bc008a56344b49c842617042641
Samuel Butler <sambutler1017@icloud.com>
2022-08-04 10:36:29 -0400

    no message

--- projects/insite-kit/src/lib/service/interceptor/http-interceptor.service.spec.ts
+++ projects/insite-kit/src/lib/service/interceptor/http-interceptor.service.spec.ts
@@ -1,18 +1,59 @@
+import { HttpHandler, HttpRequest } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { InsiteTestBed } from '../../test/insite-test-bed';
+import { InisteTestData } from '../../test/test-data';
 import { setupTests } from '../../test/test-setup';
 import { BasicHttpInterceptorService } from './http-interceptor.service';
 
-describe('BasicHttpInterceptorService', () => {
+fdescribe('BasicHttpInterceptorService', () => {
   let service: BasicHttpInterceptorService;
+  let handler: HttpHandler;
 
   setupTests(async () => InsiteTestBed.setup());
 
   beforeEach(() => {
     service = TestBed.inject(BasicHttpInterceptorService);
+    handler = TestBed.inject(HttpHandler);
+
+    spyOn(handler, 'handle');
   });
 
   it('should create service', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should handle the request when user has token', (done) => {
+    spyOn(window.localStorage, 'getItem').and.returnValue(
+      InisteTestData.getToken()
+    );
+
+    const request = new HttpRequest<any>('GET', 'fake-url');
+    const next: HttpHandler = {
+      handle(req: any) {
+        return of(req);
+      },
+    };
+
+    service.intercept(request, next).subscribe((res: any) => {
+      const authHeader: any = res.headers.lazyUpdate[0];
+      expect(authHeader.name).toEqual('Authorization');
+      expect(authHeader.value).toEqual(`Bearer: ${InisteTestData.getToken()}`);
+      done();
+    });
+  });
+
+  it('should handle the request when user is NOT authenticated', (done) => {
+    const request = new HttpRequest<any>('GET', 'fake-url');
+    const next: HttpHandler = {
+      handle(req: any) {
+        return of(req);
+      },
+    };
+
+    service.intercept(request, next).subscribe((res: any) => {
+      expect(res.headers.lazyUpdate).toBeNull();
+      done();
+    });
   });
 });
--- projects/insite-kit/src/lib/service/interceptor/http-interceptor.service.ts
+++ projects/insite-kit/src/lib/service/interceptor/http-interceptor.service.ts
@@ -1,15 +1,29 @@
 import {
+  HttpEvent,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root',
 })
 export class BasicHttpInterceptorService implements HttpInterceptor {
-  constructor() {}
-  intercept(req: HttpRequest<any>, next: HttpHandler) {
+  /**
+   * Http interceptor for attaching user authentication token to the
+   * request that are sent out. This will only be called on endpoints
+   * that need authenticated.
+   *
+   * @param req The request that is meant to be sent to the api.
+   * @param next The handler that will process the request.
+   * @returns an Observable of the http event with the authorization token.
+   */
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
     if (localStorage.getItem('token')) {
       req = req.clone({
         setHeaders: {
